bd770af43892162be01fce0c69f0d972c40640ba
speed <43330808+jsinitx4@users.noreply.github.com>
2019-03-05 18:38:25 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+TOKEN = ' '
+
+client = commands.Bot(command_prefix = 'c!')
+
+@client.event
+async def on_ready():
+    print("ready to die")
+    await client.change_presence(status=discord.Status.dnd, activity = discord.Activity(name="little t bg media scary moments", url="https://www.twitch.tv/monstercat", type=discord.ActivityType.streaming))
+
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send("something happened: `{}`".format(error))
+
+client.run(TOKEN)
