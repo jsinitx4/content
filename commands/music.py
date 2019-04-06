ce18ae0e6519a9ae8e6fddde4eff3f5a55f7ccdd
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-05 21:14:59 -0400

    play pause resume
    some of the play command was used from https://github.com/Rapptz/discord.py/blob/rewrite/examples/basic_voice.py and without it i doubt i'd get any working code

--- commands/music.py
+++ commands/music.py
@@ -1,6 +1,46 @@
-# literally useless
+import asyncio
 import discord 
+import youtube_dl
+
 from discord.ext import commands
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' 
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 class Music(commands.Cog):
     def __init__(self, client):
@@ -11,6 +51,29 @@ class Music(commands.Cog):
         """connects bot to vc"""
         await ctx.author.voice.channel.connect()
         await ctx.send("i'm in")
+
+    @commands.command(aliases=['p'])
+    async def play(self, ctx, *, url):
+        """plays a song"""
+        await ctx.channel.trigger_typing()
+        if ctx.voice_client is None:
+            await ctx.author.voice.channel.connect()
+        source = await YTDLSource.from_url(url, loop=self.bot.loop)
+        ctx.voice_client.play(source, after=lambda e: print('%s' % e) if e else None)
+        requester = ctx.author
+        await ctx.send('playing: ' + "**" + f"{source.title}" + "**" + ' requester: ' + "**" + f"{requester}" + "**")
+
+    @commands.command()
+    async def pause(self, ctx):
+        """pauses the playing song"""
+        ctx.voice_client.pause()
+        await ctx.send("done paused")
+
+    @commands.command()
+    async def resume(self, ctx):
+        """resumes playing the paused song"""
+        ctx.voice_client.resume()
+        await ctx.send("done resumed")
 
     @commands.command(aliases=['disconnect'])
     async def leave(self, ctx):
