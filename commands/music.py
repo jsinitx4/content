ec07eb7836fa1020f1de99ac1814db2fcd2cf037
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-06 10:55:12 -0400

    fix

--- commands/music.py
+++ commands/music.py
@@ -25,7 +25,7 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
+    def __init__(self, source, *, data, volume=0.7):
         super().__init__(source, volume)
         self.data = data
         self.title = data.get('title')
