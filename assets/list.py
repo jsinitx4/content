29006346b59d6a94aff468538c1847424baa61be
speed <43330808+jsinitx4@users.noreply.github.com>
2019-04-14 19:14:45 -0400

    quick flex

--- assets/list.py
+++ assets/list.py
@@ -16,3 +16,23 @@ emo = [
     "speed is marco 2",
     "who wants some nsfw memes"
 ]
+
+superior = [
+    "brooklyn superior",
+    "hi seth",
+    "Rhyfomos",
+    "remove robin",
+    "i love being a mom but it's tough when my kids come home filthy from radical islamic terrorism",
+    ":^)",
+    "irrelevant emo <@372078453236957185>",
+    "square emo european",
+    "wholesome ops :pray:",
+    "<:okretard:517885620048363523>",
+    "Bored? Eat your cum",
+    "remove bronx",
+    "seth is still sucking his mic",
+    "retard",
+    "ok, mod",
+    ".ahegao @@@@every@@one",
+    "shut up square"
+]
